27b1f0feeb9aa3bc053ddf228c8f3de8a3e69275
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-03-27 01:21:08 +0530

    Create PatienceAPI.swift

---
+++ Week-9/PatienceAPI.swift
@@ -0,0 +1,5 @@
+//  Task for Folks: Create DataModel & call API successfully!
+//
+//  API: https://pokeapi.co/api/v2/pokemon/?limit=15
+//      Create proper dataModel using this new API.
+//      Create new func in which you call this api and print data successfully in console
