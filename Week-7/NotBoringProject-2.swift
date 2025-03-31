7ab94c89b9490d37b389696044802481d553918e
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-04-01 00:49:01 +0530

    added new api end-points

--- Week-7/NotBoringProject-2.swift
+++ Week-7/NotBoringProject-2.swift
@@ -1,11 +1,12 @@
 // create struct called Song with following parameters:
-//  1. name
+//  1. songName
 //  2. artist
 //  3. duration
 
 // create struct called Playlist with following parameters:
 //  1. songs (array of Song)
-//  2. duration
+//  2. playlistName
+//  3. playlistDuration
 
 //take all the inputs from user and print them at every iterations
 //  print: playlist details on every entry
--- Week-9/PatienceAPI.swift
+++ Week-9/PatienceAPI.swift
@@ -3,3 +3,6 @@
 //  API: https://pokeapi.co/api/v2/pokemon/?limit=15
 //      Create proper dataModel using this new API.
 //      Create new func in which you call this api and print data successfully in console
+
+// New day, new API!
+//  https://api.spacexdata.com/v4/launches
